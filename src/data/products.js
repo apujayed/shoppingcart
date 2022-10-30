96820921d552d24e3a0f8a1e7886291383d8c366
Jayed Apu <95185551+apujayed@users.noreply.github.com>
2022-10-30 15:54:00 +0600

    Initial commit

---
+++ src/data/products.js
@@ -0,0 +1,68 @@
+const _products = [
+  {
+    id: 1,
+    title: "Redmi K30 至尊纪念版",
+    cover:
+      "https://cdn.cnbj0.fds.api.mi-img.com/b2c-shopapi-pms/pms_1597138340.1852584.jpg",
+    price: 2199,
+    currency: "CNY"
+  },
+  {
+    id: 2,
+    title: "小米10",
+    cover:
+      "https://cdn.cnbj0.fds.api.mi-img.com/b2c-shopapi-pms/pms_1581494045.31058391.jpg",
+    price: 3799,
+    currency: "CNY"
+  },
+  {
+    id: 3,
+    title: "小米MIX Alpha",
+    cover:
+      "https://cdn.cnbj0.fds.api.mi-img.com/b2c-shopapi-pms/pms_1569297752.58027060.jpg",
+    price: 19999,
+    currency: "CNY"
+  },
+  {
+    id: 4,
+    title: "小米电视大师 82英寸至尊纪念版",
+    cover:
+      "https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/597dc0f15e44d85928711966d540ed71.jpg",
+    price: 49999,
+    currency: "CNY"
+  },
+  {
+    id: 5,
+    title: "RedmiBook 16",
+    cover:
+      "https://cdn.cnbj0.fds.api.mi-img.com/b2c-shopapi-pms/pms_1594107583.22838030.jpg",
+    price: 5699,
+    currency: "CNY"
+  },
+  {
+    id: 6,
+    title: "Redmi小爱触屏音箱 8英寸",
+    cover:
+      "https://cdn.cnbj0.fds.api.mi-img.com/b2c-shopapi-pms/pms_1584930592.91415090.jpg",
+    price: 349,
+    currency: "CNY"
+  },
+  {
+    id: 7,
+    title: "米家互联网洗烘一体机10kg",
+    cover:
+      "https://cdn.cnbj0.fds.api.mi-img.com/b2c-shopapi-pms/pms_1545975724.92496169.jpg",
+    price: 2099,
+    currency: "CNY"
+  },
+  {
+    id: 8,
+    title: "米家风冷对开门冰箱 483L",
+    cover:
+      "https://cdn.cnbj0.fds.api.mi-img.com/b2c-shopapi-pms/pms_1570670535.55025901.jpg",
+    price: 2299,
+    currency: "CNY"
+  }
+];
+
+export default _products;
